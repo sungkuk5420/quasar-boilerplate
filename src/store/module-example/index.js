777aebb0a37e63de4a89e0412430dc022b16e77e
sungkuk5420 <sungkuk5420@gmail.com>
2020-05-11 19:11:39 +0900

    Vuex 호출 방법 변경

--- src/store/module-example/index.js
+++ src/store/module-example/index.js
@@ -1,10 +1,10 @@
 import state from "./state";
-import * as getters from "./getters";
-import * as mutations from "./mutations";
-import * as actions from "./actions";
+import { mutations } from "./mutations";
+import { getters } from "./getters";
+import { actions } from "./actions";
 
 export default {
-  namespaced: true,
+  namespaced: false,
   getters,
   mutations,
   actions,
